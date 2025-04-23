ad84cf90c95830706c1247db16f8d4477db2b626
Soham Kundu <sohamkundu29012002@gmail.com>
2025-04-23 21:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	let arr = []
+	while(str.length != 0){
+		let result = str.slice(0,size);  
+		arr.push(result);
+		str = str.slice(size)
+	} 
+	return arr 
 }
 
 // Do not change the code below
